805c7de6d1603fa38148cea478655e79fb2a6e99
Abhishek <abhiswain.it.2016@gmail.com>
2021-04-06 22:43:59 +0530

    Update code and readme

--- Mnist/Mnist-Cpp.cpp
+++ Mnist/Mnist-Cpp.cpp
@@ -1,12 +1,21 @@
-#include <iostream>
+#pragma cling add_library_path("/home/abhishek/Downloads/libtorch-cxx11-abi-shared-with-deps-1.8.1+cpu/libtorch/lib/")
+#pragma cling add_include_path("/home/abhishek/Downloads/libtorch-cxx11-abi-shared-with-deps-1.8.1+cpu/libtorch/include")
+#pragma cling add_include_path("/home/abhishek/Downloads/libtorch-cxx11-abi-shared-with-deps-1.8.1+cpu/libtorch/include/torch/csrc/api/include/")
+
+#pragma cling load("/home/abhishek/Downloads/libtorch-cxx11-abi-shared-with-deps-1.8.1+cpu/libtorch/lib/libc10.so")
+#pragma cling load("/home/abhishek/Downloads/libtorch-cxx11-abi-shared-with-deps-1.8.1+cpu/libtorch/lib/libgomp-75eea7e8.so.1")
+#pragma cling load("/home/abhishek/Downloads/libtorch-cxx11-abi-shared-with-deps-1.8.1+cpu/libtorch/lib/libtorch.so")
+
 #include <torch/torch.h>
+#include <iostream>
+#include <typeinfo>
 
 torch::Tensor Flatten(torch::Tensor x){
     return x.view({x.sizes()[0], -1});
 }
 
-struct AlexNet: torch::nn::Module {
-    AlexNet()
+struct Net: torch::nn::Module {
+    Net()
         : conv1(torch::nn::Conv2dOptions(1, 6, 3)), 
     conv2(torch::nn::Conv2dOptions(6, 16, 3)), 
     fc1(576, 120), 
@@ -35,17 +44,40 @@ struct AlexNet: torch::nn::Module {
     torch::nn::Linear fc3;
 }
 
-AlexNet model;
+Net model;
 
 torch::Tensor ip = torch::randn({1, 1, 32, 32});
 auto output = model.forward(ip);
 
 std::cout << output;
 
-void train(){
-    
+template<typename DataLoader>
+void trainer(DataLoader& loader, Net& net){
+    for(auto &batch: loader){
+        auto images = batch.data, targets = batch.target;
+        std::cout << images.sizes() << " " << targets.sizes(); 
+    }
 }
 
 void test(){
     
 }
+
+auto train_dataset = torch::data::datasets::MNIST("./data");
+
+const size_t train_dataset_size = train_dataset.size().value();
+
+std::cout << train_dataset_size; 
+
+auto item = train_dataset.get(0)
+
+std::cout << item.data.sizes();
+
+auto train_loader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
+    std::move(train_dataset), 4);
+
+auto& batch = *train_loader;
+
+typeid(batch).name()
+
+
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Work in progress repository to track progress learning libtorch
 ## Reference
 
 [Offcial examples](https://github.com/pytorch/examples/tree/master/cpp) 
+
+[Pytorch C++ frontend design and philosophy](https://pytorch.org/tutorials/advanced/cpp_frontend.html#running-the-network-in-forward-mode)
