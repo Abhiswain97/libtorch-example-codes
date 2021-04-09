c737f3800c370616a6011994439afa2d616eef18
Abhishek <abhiswain.it.2016@gmail.com>
2021-04-09 17:08:33 +0530

    Mnist jupyter notebok

---
+++ Mnist/run.sh
@@ -0,0 +1,14 @@
+if [ -d "./build" ]; then
+	echo "Build folder exists, rebuilding and executing"
+	cd build
+	cmake -DCMAKE_PREFIX_PATH=~/libtorch ..
+	make
+	./Mnist 
+else
+	echo "Creating build folder and building the project"
+	mkdir build
+	cd build
+	cmake -DCMAKE_PREFIX_PATH=~/libtorch ..
+	make 
+	./Mnist
+fi
