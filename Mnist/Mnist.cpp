9c0f94a99682af0c95affaf2f4714cff955db048
Abhishek <abhiswain.it.2016@gmail.com>
2021-04-11 04:21:20 +0530

    Training code done!

--- Mnist/Mnist.cpp
+++ Mnist/Mnist.cpp
@@ -1,5 +1,5 @@
 #include <torch/torch.h>
-#include <torch/script.h> // One-stop header.
+#include <torch/script.h>
 
 struct Options
 {
@@ -24,18 +24,53 @@ int main()
         std::move(train_dataset),
         torch::data::DataLoaderOptions().batch_size(options.batch_size).workers(2));
 
-    // Load the resnet50 model
-    torch::jit::script::Module model = torch::jit::load("resnet50.pt");
+    // Load the resne18 model
+    torch::jit::script::Module model = torch::jit::load("/home/abhishek/Desktop/libtorch-example-codes/Mnist/resnet18.pt");
 
     model.to(device);
 
-    auto optimizer = torch::optim::SGD(model.parameters(), torch::optim::SGDOptions(1e-2).momentum(0.5));
+    torch::nn::Linear fc(512, 10);
+
+    fc->to(device);
+
+    auto optimizer = torch::optim::SGD(fc->parameters(), torch::optim::SGDOptions(1e-2).momentum(0.5));
+
+    const size_t train_dataset_size = train_dataset.size().value();
+
+    // std::cout << train_dataset_size << std::endl;
 
     for (size_t i = 0; i < options.epochs; ++i)
     {
+        int64_t batch_index = 0;
+
         for (auto &batch : *train_loader)
         {
-            auto data = batch.data, targets = batch.target;
+            auto data = batch.data.to(device);
+            auto target = batch.target.to(device);
+
+            std::vector<torch::jit::IValue> input;
+            input.push_back(data);
+            optimizer.zero_grad();
+
+            auto output = model.forward(input).toTensor();
+            // For transfer learning
+            output = output.view({output.size(0), -1});
+            output = fc->forward(output);
+
+            auto loss = torch::nll_loss(torch::log_softmax(output, 1), target);
+
+            if (batch_index++ % 10 == 0)
+            {
+                std::printf(
+                    "\rTrain Epoch: %ld [%5ld/%d] Loss: %.4f",
+                    i + 1,
+                    batch_index * batch.data.size(0),
+                    60000,
+                    loss.template item<float>());
+            }
+
+            loss.backward();
+            optimizer.step();
         }
     }
 }
--- Mnist/convert_model.py
+++ Mnist/convert_model.py
@@ -1,9 +1,12 @@
-from torchvision.models import resnet50
+from torchvision.models import resnet18
+import torch.nn as nn
 import torch
 
-model = resnet50(pretrained=True)
 # 224 is the least input size, depends on the dataset you use
-example_input = torch.randn(1, 3, 224, 224)
+resnet18 = resnet18(pretrained=True)
+resnet18.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7))
+resnet18 = nn.Sequential(*list(resnet18.children())[:-1])
+example_input = torch.randn(1, 1, 224, 224)
 
-script_module = torch.jit.trace(model, example_input)
-script_module.save('resnet50.pt')
+script_module = torch.jit.trace(resnet18, example_input)
+script_module.save('resnet18.pt')
