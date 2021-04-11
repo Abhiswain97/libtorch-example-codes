fd1eeb7a727300ac1783f9bac05e1050df9c8ae0
Abhishek <abhiswain.it.2016@gmail.com>
2021-04-11 15:54:11 +0530

    remove data from tracking

--- Mnist/Mnist.py
+++ Mnist/Mnist.py
@@ -6,6 +6,17 @@ from torchvision.models import resnet18
 class Net(nn.Module):
     def __init__(self, transform=None):
         super(Net, self).__init__()
-        model = resnet18(pretrained=True)
-        model.conv1 = nn.Conv2d(
-            1, model.conv1.out_channels, kernel_size=model.conv1.kernel_size)
+        self.model = resnet18(pretrained=True)
+        self.model.conv1 = nn.Conv2d(
+            in_channels=1, out_channels=self.model.conv1.out_channels,
+            kernel_size=self.model.conv1.kernel_size
+        )
+        self.model.fc = nn.Linear(512, 10)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+net = Net()
+
+print(net)
