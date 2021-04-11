76e9b972278e31293c1a9e70e5df31e814275163
Abhishek <abhiswain.it.2016@gmail.com>
2021-04-11 19:10:14 +0530

    corrected run.sh

--- Mnist/run.sh
+++ Mnist/run.sh
@@ -1,14 +1,14 @@
 if [ -d "./build" ]; then
 	echo "Build folder exists, rebuilding and executing"
 	cd build
-	cmake -DCMAKE_PREFIX_PATH=~/libtorch ..
+	cmake -DCMAKE_PREFIX_PATH=$1 ..
 	make
 	./Mnist 
 else
 	echo "Creating build folder and building the project"
 	mkdir build
 	cd build
-	cmake -DCMAKE_PREFIX_PATH=~/libtorch ..
+	cmake -DCMAKE_PREFIX_PATH=$1 ..
 	make 
 	./Mnist
 fi
