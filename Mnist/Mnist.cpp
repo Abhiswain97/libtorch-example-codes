b542b495364dcc950329aa6f5416b24f49b049b0
Abhishek <abhiswain.it.2016@gmail.com>
2021-04-12 16:13:18 +0530

    Complete restructure of code

--- Mnist/Mnist.cpp
+++ Mnist/Mnist.cpp
@@ -1,5 +1,6 @@
 #include <torch/torch.h>
 #include <torch/script.h>
+#include <map>
 #include <chrono>
 
 using namespace torch::data;
@@ -7,8 +8,8 @@ using namespace std;
 
 struct Options
 {
-    size_t epochs = 10;
-    int64_t batch_size = 64;
+    size_t epochs = 5;
+    int64_t batch_size = 128;
     const char *data_path = "/home/abhishek/Desktop/libtorch-example-codes/Mnist/data";
     datasets::MNIST::Mode train = datasets::MNIST::Mode::kTrain;
     datasets::MNIST::Mode test = datasets::MNIST::Mode::kTest;
@@ -21,12 +22,12 @@ int main()
 
     if (torch::cuda::is_available())
     {
-        printf("Cuda is available!");
+        std::printf("Cuda is available!\n\n");
         device_type = torch::kCUDA;
     }
     else
     {
-        printf("Cuda is not available!");
+        std::printf("Cuda is not available!\n\n");
         device_type = torch::kCPU;
     }
 
@@ -36,6 +37,7 @@ int main()
     Making the dataset and Data Loader
     ----------------------------------------------------------------
     */
+
     auto train_dataset = datasets::MNIST(
                              options.data_path, options.train)
                              .map(transforms::Normalize<>(0.1307, 0.3081))
@@ -51,6 +53,8 @@ int main()
     auto test_loader = make_data_loader(
         move(test_dataset),
         DataLoaderOptions().batch_size(options.batch_size).workers(2));
+
+    vector<string> modes = {"Training", "Testing"};
 
     /*
     Load the jit resne18 model
@@ -69,115 +73,83 @@ int main()
 
     auto optimizer = torch::optim::SGD(fc->parameters(), torch::optim::SGDOptions(1e-2).momentum(0.5));
 
-    const size_t train_dataset_size = train_dataset.size().value();
+    auto criterion = torch::nn::CrossEntropyLoss();
 
-    /*
-    Training the model
-    ---------------------------------------------------------------- 
-    */
+    map<string, int> sizes;
+    sizes["Training"] = 60000;
+    sizes["Testing"] = 10000;
+
+    double best_loss = 0.0;
 
     auto start_training = chrono::high_resolution_clock::now();
 
-    fc->train();
-
     for (size_t i = 0; i < options.epochs; ++i)
     {
-        int64_t batch_index = 0;
-        double avg_loss;
-
-        for (auto &batch : *train_loader)
+        for (string &mode : modes)
         {
-            auto data = batch.data.to(device);
-            auto target = batch.target.to(device);
+            auto &data_loader = mode == string("Training") ? *train_loader : *test_loader;
 
-            vector<torch::jit::IValue> input;
-            input.push_back(data);
-            optimizer.zero_grad();
-
-            auto output = model.forward(input).toTensor();
-            // For transfer learning
-            output = output.view({output.size(0), -1});
-            output = fc->forward(output);
-
-            auto loss = torch::nll_loss(torch::log_softmax(output, 1), target);
-
-            avg_loss += loss.template item<float>();
-
-            if (batch_index++ % 10 == 0)
+            if (mode == string("Training"))
+                fc->train();
+            else
             {
-                printf(
-                    "\rTrain Epoch: %ld [%5ld/%d] Loss: %.4f",
-                    i + 1,
-                    batch_index * batch.data.size(0),
-                    60000,
-                    loss.template item<float>());
+                torch::NoGradGuard no_grad_guard;
+                fc->eval();
+            };
+
+            int64_t batch_index = 0;
+            double avg_loss;
+
+            for (auto &batch : data_loader)
+            {
+                auto data = batch.data.to(device);
+                auto target = batch.target.to(device);
+
+                vector<torch::jit::IValue> input;
+                input.push_back(data);
+
+                if (mode == string("Training"))
+                    optimizer.zero_grad();
+
+                auto output = model.forward(input).toTensor();
+
+                output = output.view({output.size(0), -1});
+                output = fc->forward(output);
+
+                auto loss = criterion(output, target);
+
+                if (mode == string("Training"))
+                {
+                    loss.backward();
+                    optimizer.step();
+                }
+                else
+                    avg_loss += loss.template item<float>();
+
+                if (batch_index++ % 10 == 0)
+                {
+                    std::printf(
+                        "\r%s Epoch: %ld [%5ld/%d] Loss: %.4f",
+                        mode.c_str(),
+                        i + 1,
+                        batch_index * batch.data.size(0),
+                        sizes[mode],
+                        loss.template item<float>());
+                }
             }
 
-            loss.backward();
-            optimizer.step();
-        }
+            if (mode == string("Training"))
+                std::printf("\nAverage test loss for epoch %zu: %.4f\n", i, avg_loss / (double)sizes["Testing"]);
 
-        printf("\nAverage training loss for epoch %zu: %.4f\n", i, avg_loss / 60000);
+            if (avg_loss < best_loss)
+                torch::save(fc, "../best_model.pt");
+        }
     }
 
     auto end_training = chrono::high_resolution_clock::now();
 
-    chrono::duration<float> train_duration = end_training - start_training;
+    chrono::duration<float> test_duration = end_training - start_training;
 
-    cout << "\nTraining completed in: " << train_duration.count() << "s"
-         << "\n";
-
-    /*
-    Testing the model
-    ---------------------------------------------------------------- 
-    */
-
-    auto start_testing = chrono::high_resolution_clock::now();
-
-    torch::NoGradGuard no_grad_guard;
-    fc->eval();
-
-    for (size_t i = 0; i < options.epochs; ++i)
-    {
-        int64_t batch_index = 0;
-        double avg_loss;
-
-        for (auto &batch : *test_loader)
-        {
-            auto data = batch.data.to(device);
-            auto target = batch.target.to(device);
-
-            vector<torch::jit::IValue> input;
-            input.push_back(data);
-
-            auto output = model.forward(input).toTensor();
-
-            output = output.view({output.size(0), -1});
-            output = fc->forward(output);
-
-            auto loss = torch::nll_loss(torch::log_softmax(output, 1), target);
-            avg_loss += loss.template item<float>();
-
-            if (batch_index++ % 10 == 0)
-            {
-                printf(
-                    "\rTest Epoch: %ld [%5ld/%d] Loss: %.4f",
-                    i + 1,
-                    batch_index * batch.data.size(0),
-                    10000,
-                    loss.template item<float>());
-            }
-        }
-        printf("\nAverage training loss for epoch %zu: %.4f\n", i, avg_loss / 10000);
-
-        cout << "\n Saving model after epoch: " << i << "\n";
-        torch::save(fc, "../best_model.pt");
-    }
-
-    auto end_testing = chrono::high_resolution_clock::now();
-
-    chrono::duration<float> test_duration = end_testing - start_testing;
-
-    cout << "\nTesting completed in: " << test_duration.count() << "s"
-         << "\n";
+    std::cout << "\nTraining completed in: " << test_duration.count() << "s"
+              << "\n";
 }
